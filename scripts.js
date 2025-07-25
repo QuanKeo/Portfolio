e3833bbfac373e9319062a8f8a7fd5b60a3532f1
QuanKeo <quan300402@gmail.com>
2025-07-25 10:11:04 +0700

    new

--- scripts.js
+++ scripts.js
@@ -1,3 +1,22 @@
 function toggleMobileMenu() {
     document.getElementById("menu").classList.toggle("active");
-}
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    window.onscroll = function() {
+        var btn = document.getElementById("backToTop");
+        if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+            btn.style.display = "block";
+        } else {
+            btn.style.display = "none";
+        }
+    };
+
+    document.getElementById("backToTop").onclick = function() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+});
+
