4618fd61bd17dd2aa77b3201ec06f1cc488d59cb
QuanKeo <quan300402@gmail.com>
2025-07-25 11:18:30 +0700

    new

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function toggleMobileMenu() {
 document.addEventListener("DOMContentLoaded", function() {
     window.onscroll = function() {
         var btn = document.getElementById("backToTop");
-        if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
             btn.style.display = "block";
         } else {
             btn.style.display = "none";
